162c3fb67789caca4e39567ef6d3d3fca5cb58c7
joschiservice <joschua.hass.sh@gmail.com>
2021-05-19 15:33:00 +0200

    worked on product model

---
+++ app/Models/Store/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Store;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ */
+class Product extends Model
+{
+    use HasFactory;
+}
