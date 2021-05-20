4cea49eb251259283e2a0c38860b60de885cba2d
joschiservice <joschua.hass.sh@gmail.com>
2021-05-20 17:19:20 +0200

    added route to return products of a team

--- app/Http/Controllers/Store/TeamController.php
+++ app/Http/Controllers/Store/TeamController.php
@@ -5,7 +5,17 @@ namespace App\Http\Controllers\Store;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Models\Store\Team;
+
 class TeamController extends Controller
 {
-    //
+    /**
+     * Display the products of the team.
+     *
+     * @param int $id ID of the team
+     * @return \Illuminate\Http\Response
+     */
+    public function products(int $id) {
+        return Team::findorfail($id)->products()->get();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,8 @@ Route::middleware('auth:sanctum')->get('/user/me', function (Request $request) {
 });
 
 Route::prefix('store')->name('store.')->group(function(){
+    Route::prefix('team')->name('store.')->group(function(){
+        Route::get('{id}/products', [\App\Http\Controllers\Store\TeamController::class, 'products']);
+    });
     Route::resource('products', \App\Http\Controllers\Store\ProductController::class);
 });
