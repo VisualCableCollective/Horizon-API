a5da99dc3d7e179451c0ec7273d71a4f0cd5b4be
joschiservice <joschua.hass.sh@gmail.com>
2021-05-20 16:43:01 +0200

    added product resource to api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user/me', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('store')->name('store.')->group(function(){
+    Route::resource('products', \App\Http\Controllers\ProductController::class);
+});
